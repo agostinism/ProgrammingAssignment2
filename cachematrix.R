2e1984cd575a9eae460b0765ff1e8eafda1fef83
Simon Mendoza Agostini <simon.mendoza@gmail.com>
2014-09-20 10:51:45 -0400

    Cache Matrix Assignment, 2 functions
    This program has two functions defined: makeCacheMatrix and cacheSolve
    Both receive as input a Identity qualified matrix
    makeCacheMatrix will keep the Identity Matrix, if generated and will
    retrieve it
    by getmatriz() call. If no prior Identity matrix has been generated NULL
    will be displayed
    cacheSolve() will generate the identity matrix retrievable by
    getmatriz() function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This program has two functions defined: makeCacheMatrix and cacheSolve
+## Both receive as input a Identity qualified matrix
+## makeCacheMatrix will keep the Identity Matrix, if generated and will retrieve it 
+## by getmatriz() call. If no prior Identity matrix has been generated NULL will be displayed
+## cacheSolve() will generate the identity matrix retrievable by getmatriz() function
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Function that sets the Identity matrix in cache, retrievable 
+ 
+makeCacheMatrix <- function(x = Numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatriz <- function(solvex) m <<- solvex
+        getmatriz <- function() m
+        list(set = set, get = get,
+             setmatriz = setmatriz,
+             getmatriz = getmatriz)
 }
 
-
-## Write a short comment describing this function
-
+## Function that will get the Identity Matrix if it has not been generated, otherwise returns the ## one in Cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatriz()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data) %*% data
+        x$setmatriz(m)
+        m
 }
